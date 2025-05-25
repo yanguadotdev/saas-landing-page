afa32c1f7a21ec3cde0a29746bd44952abb3515e
yanguadotdev <yanguasamir05@gmail.com>
2025-05-24 21:23:51 -0500

    feat(LogoTicker): implement animated logo ticker with motion effects

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import quantumLogo from '@/assets/quantum.svg'
 import acmeLogo from '@/assets/acme-corp.svg'
 import echoValleyLogo from '@/assets/echo-valley.svg'
@@ -7,6 +9,8 @@ import apexLogo from '@/assets/apex.svg'
 import celestialLogo from '@/assets/celestial.svg'
 import twiceLogo from '@/assets/twice.svg'
 import Image from 'next/image'
+import { Fragment } from 'react'
+import { motion } from 'motion/react'
 
 const logos = [
   { name: 'Quantum', image: quantumLogo },
@@ -23,17 +27,27 @@ export default function LogoTicker() {
   return (
     <section className='overflow-x-clip py-24'>
       <div className='container mx-auto'>
-        <h3 className='text-center text-white/60 text-xl'>Already chosen by these market leaders</h3>
-        <div className='overflow-hidden mt-12 [mask-image:linear-gradient(to_right,black_10%,black_90%,transparent)]'>
-          <div className='flex gap-24 pr-24'>
-            {logos.map((logo) => (
-              <Image
-                src={logo.image}
-                alt={`${logo.name} logo`}
-                key={logo.name}
-              />
+        <h3 className='text-center text-white/60 text-xl'>
+          Already chosen by these market leaders
+        </h3>
+        <div className='flex overflow-hidden mt-12 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]'>
+          <motion.div
+            animate={{ x: '-50%' }}
+            transition={{ duration: 25, repeat: Infinity, ease: 'linear' }}
+            className='flex flex-none gap-24 pr-24'
+          >
+            {Array.from({ length: 2 }).map((_, index) => (
+              <Fragment key={index}>
+                {logos.map((logo) => (
+                  <Image
+                    src={logo.image}
+                    alt={`${logo.name} logo`}
+                    key={logo.name}
+                  />
+                ))}
+              </Fragment>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
