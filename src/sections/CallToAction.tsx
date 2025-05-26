19bdcbba3cce69f13f11e3a00feb9cec7ad28239
yanguadotdev <yanguasamir05@gmail.com>
2025-05-25 22:13:41 -0500

    feat(CallToAction): add hover effects and animation to the call-to-action section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,15 +1,44 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client'
+
+import { AnimationPlaybackControls, motion, useAnimate } from 'motion/react'
+import { useEffect, useRef, useState } from 'react'
+
 export default function CallToAction() {
+  const [isHovered, setIsHovered] = useState(false)
+  const animation = useRef<AnimationPlaybackControls>(null)
+  const [scope, animate] = useAnimate()
+
+  useEffect(() => {
+    animation.current = animate(
+      scope.current,
+      { x: '-50%' },
+      { duration: 25, ease: 'linear', repeat: Infinity }
+    )
+  }, [])
+
+  useEffect(() => {
+    if (animation.current) {
+      animation.current.speed = isHovered ? 0.5 : 1
+    }
+  }, [isHovered])
+
   return (
     <section className='py-24'>
-      <div className='overflow-x-clip p-4'>
-        <div className="flex flex-none gap-16 text-7xl font-medium md:text-8xl whitespace-nowrap">
+      <div className='overflow-x-clip p-4 flex'>
+        <motion.div
+          ref={scope}
+          className='flex flex-none gap-16 text-7xl font-medium md:text-8xl whitespace-nowrap group'
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
           {Array.from({ length: 10 }).map((_, index) => (
-            <div key={index} className="flex items-center gap-16">
-              <span className="text-lime-400 text-7xl">&#10038;</span>
-              <span>Try it for free</span>
+            <div key={index} className='flex items-center gap-16'>
+              <span className='text-lime-400 text-7xl'>&#10038;</span>
+              <span className='group-hover:text-lime-400'>Try it for free</span>
             </div>
           ))}
-        </div>
+        </motion.div>
       </div>
     </section>
   )
