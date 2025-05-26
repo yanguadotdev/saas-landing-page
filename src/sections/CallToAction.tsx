460611490503470e6b392f4c61449b49821205a3
yanguadotdev <yanguasamir05@gmail.com>
2025-05-26 11:49:35 -0500

    feat: add custom cursor animation with floating text on CTA section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,13 +1,40 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 'use client'
 
-import { AnimationPlaybackControls, motion, useAnimate } from 'motion/react'
+import {
+  AnimationPlaybackControls,
+  motion,
+  useAnimate,
+  useMotionValue
+} from 'motion/react'
 import { useEffect, useRef, useState } from 'react'
 
 export default function CallToAction() {
   const [isHovered, setIsHovered] = useState(false)
   const animation = useRef<AnimationPlaybackControls>(null)
   const [scope, animate] = useAnimate()
+
+  const [width, height] = [192, 48]
+  // Cursor position
+  const sectionRef = useRef<HTMLDivElement | null>(null)
+  const mousePosition = {
+    x: useMotionValue(0),
+    y: useMotionValue(0)
+  }
+
+  // Handler for window mousemove event
+  const handleWindowMouseMove = (e: MouseEvent) => {
+    console.log('_jkhjdgh')
+    mousePosition.x.set(e.clientX - (width / 2))
+    mousePosition.y.set(e.clientY - (height / 2))
+  }
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleWindowMouseMove)
+    return () => {
+      window.removeEventListener('mousemove', handleWindowMouseMove)
+    }
+  }, [])
 
   useEffect(() => {
     animation.current = animate(
@@ -24,13 +51,16 @@ export default function CallToAction() {
   }, [isHovered])
 
   return (
-    <section className='py-24'>
-      <div className='overflow-x-clip p-4 flex'>
+    <section className='py-24 border border-white/10'>
+      <div
+        className='overflow-x-clip p-4 flex border border-white/15'
+        ref={sectionRef}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
         <motion.div
           ref={scope}
-          className='flex flex-none gap-16 text-7xl font-medium md:text-8xl whitespace-nowrap group'
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
+          className='flex flex-none gap-16 text-7xl font-medium md:text-8xl whitespace-nowrap group cursor-none'
         >
           {Array.from({ length: 10 }).map((_, index) => (
             <div key={index} className='flex items-center gap-16'>
@@ -38,6 +68,21 @@ export default function CallToAction() {
               <span className='group-hover:text-lime-400'>Try it for free</span>
             </div>
           ))}
+        </motion.div>
+
+        <motion.div
+          className={`pointer-events-none fixed top-0 left-0 flex items-center justify-center z-50 text-2xl font-semibold  rounded-full bg-white text-black`}
+          initial={false}
+          style={{
+            x: mousePosition.x,
+            y: mousePosition.y,
+            width,
+            height
+          }}
+          animate={{ scale: isHovered ? 1 : 0 }}
+          transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+        >
+          Yes, please ✨
         </motion.div>
       </div>
     </section>
